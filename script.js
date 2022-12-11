973f0bc10e8a2fa48d7c9c8939b0af320df5cea0
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2022-12-11 15:37:40 +0000

    Create eventlisteners for number buttons and clear button

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+const clear = document.querySelector(".clear");
+
+function displayCalculator() {
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      display.innerText += button.innerText;
+    });
+  console.log(display.innerText);
+});
+}
+
+function clearDisplay() {
+  display.innerText = "";
+}
+
+clear.addEventListener("click", clearDisplay);
+
+
+displayCalculator();
+
+
 function sum(a, b) {
   return a + b;
 }
@@ -21,5 +44,3 @@ function modulo(a, b) {
 function operate(operator, a, b) {
   return operator(a, b);
 }
-
-console.log(operate(modulo, 10, 2));
